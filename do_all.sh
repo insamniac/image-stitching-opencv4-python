c9d8a76ce91af73c3f07fbff53c27bce3d361bca
Sam Burke <burkesamuel@gmail.com>
2019-07-19 11:34:26 -0700

    another test run

--- do_all.sh
+++ do_all.sh
@@ -2,15 +2,15 @@
 
 
 #Take one minute chunk of video starting after 1:08
-oName='00100'
+oName="${1:-00100}"
 
 #ffmpeg -ss 00:01:08 -i ~/rov_videos_day2/2019-07-13_09.59.05.mkv -t 00:01:00 -vcodec copy videos/${oName}.mkv
 
 #Extract image every 0.5 seconds. crop 128 pixels from left and bottom (probably)
 # frames per second... (images per second)
-FPS=2
+FPS=${2:-1}
 mkdir images/${oName}
-ffmpeg -i videos/${oName}.MTS -qscale:v 2 -vf "scale=1024:-1,fps=${FPS}" images/${oName}/%05d.jpg
+ffmpeg -i videos/${oName}.MTS -qscale:v 2 -vf "rotate=90,scale=1024:-1,fps=${FPS}" images/${oName}/%05d.jpg
 
 #Move images into groups of no more than 10..
 groups=$(( $(ls images/${oName} | wc -l) / 10))
@@ -40,7 +40,7 @@ do
  if [[ $(ls images/${oName}_${i} 2>/dev/null | wc -l) -gt 0 ]] ; then
     for img in $(ls images/${oName}_${i}/)
     do
-        echo "<img src='../images/${oName}_${i}/${img}' width='64px' align='left' />" 
+        echo "<img src='../../images/${oName}_${i}/${img}' width='64px' align='left' />" 
     done >> $oLog
 
     echo "<img src='${oName}_${i}.png' alt='stitched output for ${oName}' title='stitched' />" >> $oLog
